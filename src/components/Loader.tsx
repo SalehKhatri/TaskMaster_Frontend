d6362701c54c52dd30ed7a1f428d69ecaa559f83
Saleh <salehkhatri29@gmail.com>
2024-12-15 19:35:36 +0530

    Changed loader text from 'Loading tasks...' to 'Loading...'

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -5,7 +5,7 @@ const Loader:React.FC = () => {
     return <div className="flex flex-col justify-center items-center">
         <ClipLoader color="#000" loading={true} size={50}/>
         <p className="text-center font-secondary text-gray-500">
-            Loading tasks...
+            Loading...
         </p>
     </div>
 }
