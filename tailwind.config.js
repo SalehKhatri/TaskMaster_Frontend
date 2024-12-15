fda8b9c57977d43b47b24265558a801374f8bccf
Saleh <salehkhatri29@gmail.com>
2024-12-15 19:19:53 +0530

    Fixed build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-		fontFamily: {
-			primary: ['Inter', 'sans-serif'],
-			secondary: ['Roboto', 'sans-serif'],
-		},
-  		colors: {}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-}
+import tailwindcssAnimate from "tailwindcss-animate";
 
+export default {
+	darkMode: ["class"],
+	content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+	theme: {
+		extend: {
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			fontFamily: {
+				primary: ["Inter", "sans-serif"],
+				secondary: ["Roboto", "sans-serif"],
+			},
+			colors: {},
+		},
+	},
+	plugins: [tailwindcssAnimate],
+};
